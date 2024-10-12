64f6ca30bdec01e1b7d1340aba3df6c33f8c0db3
lena_aleinikova <alenka_aleinikova@mail.ru>
2024-10-12 18:26:53 +0300

    add polygons on the map

--- app/src/main/java/com/example/velomap/MainActivity.kt
+++ app/src/main/java/com/example/velomap/MainActivity.kt
@@ -1,23 +1,65 @@
+package com.example.velomap
+
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.velomap.R
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
+import com.mapbox.maps.extension.style.layers.addLayer
+import com.mapbox.maps.extension.style.layers.generated.fillLayer
+import com.mapbox.maps.extension.style.sources.addSource
+import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mapView: MapView
+    private lateinit var style: Style
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // Инициализация MapView
         mapView = findViewById(R.id.mapView)
 
-        // Загрузка карты с указанным стилем
-        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
-    }
+        // Загрузка стиля карты
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS) { loadedStyle ->
+            // Сохраняем загруженный стиль в переменную
+            style = loadedStyle
 
+            // После загрузки стиля вызываем метод для отображения GeoJSON полигонов
+            loadGeoJson(style)
+        }
+    }
+    private fun loadGeoJson(style: Style) {
+        try {
+            val geoJsonString = assets.open("polygons.geojson").bufferedReader().use { it.readText() }
+            Log.d("GeoJSON", "Файл успешно загружен")
+
+            val geoJsonSource = geoJsonSource("polygon-source") {
+                data(geoJsonString)
+            }
+
+            // Добавляем источник на карту
+            style.addSource(geoJsonSource)
+
+            // Добавляем слой для отображения полигона
+            style.addLayer(
+                fillLayer("polygon-layer", "polygon-source") {
+                    fillColor("#ff0000") // Цвет полигона
+                    fillOpacity(0.5)    // Прозрачность полигона
+                }
+            )
+            Log.d("GeoJSON", "слой для отображения полигона")
+        } catch (e: Exception) {
+            Log.e("GeoJSON", "Ошибка при загрузке файла: ${e.message}")
+        }
+        // Создаем GeoJsonSource
+
+    }
     override fun onStart() {
         super.onStart()
         mapView.onStart()
@@ -32,6 +74,4 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         mapView.onDestroy()
     }
-
-
 }
