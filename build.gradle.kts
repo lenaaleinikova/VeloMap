3a4d419d73c79acec7f1693ea678092cb82a9767
lena_aleinikova <alenka_aleinikova@mail.ru>
2024-11-29 12:56:20 +0300

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -5,6 +7,8 @@ plugins {
 }
 buildscript {
     dependencies {
+        classpath("com.android.tools.build:gradle:8.4.1")
         classpath("com.google.gms:google-services:4.4.2") // Используйте последнюю версию
     }
 }
+
